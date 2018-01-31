8959b73c089ad7576e4f8a23a89086b6da5bb4f6
Stefano Romanazzi <stefan.romanazzi@gmail.com>
2018-01-31 21:12:34 +0100

    Data fix and coordinates loading into code
    - Datasets about downtown merged
    - Coordinates acquired from .csv

--- DownloadIMGfromCSVcoordinates.py
+++ DownloadIMGfromCSVcoordinates.py
@@ -1,19 +1,29 @@
 import requests
 import sys, csv
 from collections import defaultdict
+import numpy as np
+from decimal import Decimal
 
-Xcoord = defaultdict(list) # each value in each column is appended to a list
-Ycoord = defaultdict(list)
-kmlName = 'Zona Centro 1.kml'
+Xcoord = np.zeros(3000)
+Ycoord = np.zeros(3000)
+kmlName = 'Zona Centro.kml'
 
 
-with open(kmlName) as kml:
+with open('data/Bologna gps data/' + kmlName) as kml:
     reader = csv.DictReader(kml)  # read rows into a dictionary format
+    i = 0
     for row in reader:  # read a row as {column1: value1, column2: value2,...}
         for (k, v) in row.items():  # go over each column name and value
-            if k == 0:
-                Xcoord.append(v)  # append the value into the appropriate list
-                                      # based on column name k
+            if k == ' Xcoord':
+                Xcoord[i] = Decimal(v)
+            if k == 'Ycoord':
+                Ycoord[i] = Decimal(v)
+        i += 1
+
+Xcoord = Xcoord[:i]
+Ycoord = Ycoord[:i]
+print(Xcoord)
+print(Ycoord)
 
 
 #img_data = requests.get(image_url).content
