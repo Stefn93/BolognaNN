a4e17152308187f6ac89dd753317b680a2f70446
Stefano Romanazzi <stefan.romanazzi@gmail.com>
2018-02-01 11:19:15 +0100

    Casteldebole

--- DownloadIMGfromCSVcoordinates.py
+++ DownloadIMGfromCSVcoordinates.py
@@ -1,6 +1,5 @@
 import requests
-import sys, csv
-from collections import defaultdict
+import csv
 import numpy as np
 from pathlib import Path
 from decimal import Decimal
@@ -27,13 +26,16 @@ Ycoord = Ycoord[:coordNum]
 print(Xcoord)
 print(Ycoord)
 
-startnsize = 'https://maps.googleapis.com/maps/api/streetview?size=300x300&location='
-heading =  '&heading='
+startnsize = 'https://maps.googleapis.com/maps/api/streetview'
+size = '?size=300x300'
+location = '&location='
+heading = '&heading='
 pitch = '&pitch=0'
 APIkey = '&key=AIzaSyCKBuiVEKcPBJk3h29HQxXPOUwOb59F3Cc'
 for i in range(coordNum+1):
     for j in range(4):
-        image_url = startnsize + str(Xcoord[i-1]) + ',' + str(Ycoord[i-1]) + heading + str(j * 90) + pitch + APIkey
+        image_url = startnsize + size + location + str(Xcoord[i-1]) + ',' + str(Ycoord[i-1]) \
+                    + heading + str(j * 90) + pitch + APIkey
         fileName = 'data/Casteldebole/' + str(Xcoord[i-1]) + 'X_' + str(Ycoord[i-1]) + 'Y_' + str(j*90) + 'deg.jpg'
         if not Path(fileName).exists():
             img_data = requests.get(image_url).content
